d8eb00f5627a0179004f9bfd1dd8bc775d7b095c
NetLamp <oplpublishergerman@gmail.com>
2016-10-07 16:53:31 +0200

    Update to 0.2.9_1: Smaller and faster

--- ccl.bat
+++ ccl.bat
@@ -1,8 +1,8 @@
-java -jar ccl0.2.9.jar -compile %1
+java -jar ccl0.2.9_1.jar -compile %1
 if errorlevel 1 goto end
 set fname=%1
 set fname=%fname:~0,-1%0
-java -jar ccl0.2.9.jar %fname%
+java -jar ccl0.2.9_1.jar %fname%
 if errorlevel 1 goto end
 
 :end
