2effd9fc8ca8cd66994c108af542f6d2a2424105
NetLamp <oplpublishergerman@gmail.com>
2016-11-09 12:30:40 +0100

    Update to 0.3.2: Remove cl1 subcode
    Removed cl1 subcode to allow faster compile.
    Added string analyzer to enable unescape (\n, \t).

--- ccl.bat
+++ ccl.bat
@@ -1,8 +1,8 @@
-java -jar ccl0.3.1.jar -compile %1
+java -jar ccl0.3.2.jar -compile %1
 if errorlevel 1 goto end
 set fname=%1
 set fname=%fname:~0,-1%0
-java -jar ccl0.3.1.jar %fname%
+java -jar ccl0.3.2.jar %fname%
 if errorlevel 1 goto end
 
 :end
