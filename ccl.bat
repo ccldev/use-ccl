a434abe3aaab0f3da257540c2e5d76b37cb137c2
NetLamp <oplpublishergerman@gmail.com>
2016-11-11 16:10:59 +0100

    Update to 0.3.2_3: Plugins and Bytecode prototype.

--- ccl.bat
+++ ccl.bat
@@ -1,8 +1,8 @@
-java -jar ccl0.3.2_2.jar -compile %1
+java -jar ccl0.3.2_3.jar -compile %1 -plugins plugins
 if errorlevel 1 goto end
 set fname=%1
 set fname=%fname:~0,-1%0
-java -jar ccl0.3.2_2.jar %fname%
+java -jar ccl0.3.2_3.jar %fname%
 if errorlevel 1 goto end
 
 :end
