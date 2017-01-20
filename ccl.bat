e6b807ecce906acb70be2c8470689d5945b64449
NetLamp <oplpublishergerman@gmail.com>
2017-01-20 10:54:54 +0100

    Update to 0.3.18: Coa Runtime included

--- ccl.bat
+++ ccl.bat
@@ -1,8 +1,8 @@
-java -jar ccl0.3.17.1.jar -compile %1 -plugins plugins -head on
+java -jar ccl0.3.18.jar -compile %1 -plugins plugins -head on
 if errorlevel 1 goto end
 set fname=%1
 set fname=%fname:~0,-1%0
-java -jar ccl0.3.17.1.jar %fname%
+java -jar ccl0.3.18.jar %fname%
 if errorlevel 1 goto end
 
 :end
